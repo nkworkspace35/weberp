f73f0f4e3bde00575da371655c73633bf6fb94ab
Nikhil Verma <nk.workspace35@gmail.com>
2026-09-09 20:39:55 +0530

    updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,7 +6,7 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME || 'test',
-  port: process.env.DB_PORT || 4000,
+  port: process.env.DB_PORT || 3306,
   ssl: {
     minVersion: 'TLSv1.2',
     rejectUnauthorized: true
